cccd55b5a7028a8aa76c1d20a5cc94cec0460a87
PiotrIT2017 <piotr.prochnicki@gmail.com>
2020-05-31 08:27:14 +0200

    Feature-fix/improve project structure

--- communicationwithexternalapps/Reminder/src/main/java/com/example/controller/ReportController.java
+++ communicationwithexternalapps/Reminder/src/main/java/com/example/controller/ReportController.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.example;
+package com.example.controller;
 
-
+import com.example.service.DataService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,12 +13,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 
 @Controller
-public class Raport {
-    
+public class ReportController {
+
     @RequestMapping(value="/raport", method = RequestMethod.POST)
-    public void Generuj(@ModelAttribute("SpringWeb")Dane dane, ModelMap model){
-       model.addAttribute("dane", dane.InsertQuery());
+    public void Generuj(@ModelAttribute("SpringWeb") DataService dataService, ModelMap model){
+        model.addAttribute("dane", dataService.InsertQuery());
     }
-    
-    
+
+
 }
