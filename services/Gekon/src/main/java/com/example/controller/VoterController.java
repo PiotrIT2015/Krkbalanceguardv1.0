6d65200c8f6166b7f59fa7d3a3b34a67173e649a
PiotrIT2017 <piotr.prochnicki@gmail.com>
2020-06-14 03:19:09 +0200

    Feature-fix/add services

---
+++ services/Gekon/src/main/java/com/example/controller/VoterController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import com.example.service.VoterService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Piotr
+ */
+
+@Controller
+public class VoterController {
+
+    @RequestMapping(value="/raport", method = RequestMethod.POST)
+    public void Generuj(@ModelAttribute("SpringWeb") VoterService voterService, ModelMap model){
+        model.addAttribute("dane", voterService.InsertQuery());
+    }
+
+
+}
