da205665cc9266be166432c07340028ad89ee942
Piotr Próchnicki <piotr.prochnicki@gmail.com>
2022-02-09 22:09:43 +0100

    Feature-fix/add tests

--- io.krkbalanceguard.services/Gekon/src/test/java/engine/pp/controller/voterControllerTest.java
+++ io.krkbalanceguard.services/Gekon/src/test/java/engine/pp/controller/voterControllerTest.java
@@ -4,11 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.Assert.assertNotNull;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class voterControllerTest {
 
